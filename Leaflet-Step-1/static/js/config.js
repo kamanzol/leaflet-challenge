783aade1c75fa108ce7e15fe58c7b684bedfaf5c
kamanzol <katie.manzoline@gmail.com>
2020-04-09 21:45:23 -0500

    some updates

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2FtYW56b2wiLCJhIjoiY2s4ZjQ5N3B3MDBlczNkcWo2YndvbTJpeiJ9.D_-zJwuNEPDk5kshBWEdzA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,17 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+
+
+
+    
+}
+
